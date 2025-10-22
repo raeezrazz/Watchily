f42883a46b25ca69a16bb35a212068ef9669eb40
raeezrazz <mhdrahees67@gmail.com>
2025-10-22 16:29:20 +0530

    changed the backend url 6

--- Frontend/src/api/apiClient/axios.ts
+++ Frontend/src/api/apiClient/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'https://watchily.onrender.com',
+    baseURL: 'https://watchily.onrender.com/',
     withCredentials:true
 })
 
